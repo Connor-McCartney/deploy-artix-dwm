0fd86d2a183e4c0e4f78061ec5d542298cc53452
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2026-04-29 09:53:11 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -13,3 +13,12 @@ echo "artix" > /etc/hostname
 
 
 
+if [[ $target -eq "uefi-luks-hyperv" ]]; then
+    printf "MODULES=()\nBINARIES=()\nFILES=()\nHOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block encrypt lvm2 filesystems fsck)\n" > /etc/mkinitcpio.conf
+    pacman -S --noconfirm thin-provisioning-tools lvm2
+    mkinitcpio -p linux
+    pacman -S efibootmgr --noconfirm; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+    uuid=$(blkid -s UUID -o value /dev/sda2)
+    printf "GRUB_TIMEOUT=1\nGRUB_DISTRIBUTOR=\"Artix\"\nGRUB_CMDLINE_LINUX=\"loglevel=1 nowatchdog cryptdevice=UUID="$uuid":cryptlvm root=/dev/vg1/root nvme_load=YES fsck.mode=skip modprobe.blacklist=iTCO_wdt\"\n" > /etc/default/grub
+    grub-mkconfig -o /boot/grub/grub.cfg
+fi
