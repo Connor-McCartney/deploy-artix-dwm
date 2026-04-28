5c41bf460f77701eb06b57e0a79edb48f57361d1
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2026-04-29 09:50:47 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -2,3 +2,8 @@ set -e
 
 ln -sf /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
 hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "artix" > /etc/hostname
