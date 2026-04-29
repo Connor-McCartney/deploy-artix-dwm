20218a67f71f3b4076503430ebd0e7d75d1908e6
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2026-04-29 17:57:55 +1000

    Update user.sh

--- user.sh
+++ user.sh
@@ -2,7 +2,7 @@ set -e
 
 
 
-sudo pacman -S --noconfirm base-devel os-prober grub vlc htop git wget ripgrep fzf bat fd vim less git wget cmake
+sudo pacman -S --noconfirm base-devel os-prober grub vlc htop git wget ripgrep fzf bat fd vim less git wget cmake unzip
 
 # compile yay
 cd /tmp
