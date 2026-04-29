dfa3481f91adb0f2f1b9e7124c9925dfaf2622b1
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2026-04-29 11:08:20 +1000

    Update user.sh

--- user.sh
+++ user.sh
@@ -3,4 +3,5 @@ set -e
 sudo pacman -S --noconfirm base-devel os-prober grub vlc htop git wget ripgrep fzf bat fd vim less pavucontrol kitty
 
 sudo pacman -S --noconfirm xlibre-xserver
-sudo pacman -S --noconfirm xorg-xinit
+sudo pacman -S --noconfirm xorg-xinit xorg-xrandr
+# xrandr --output Virtual-1 --mode 1920x1080
