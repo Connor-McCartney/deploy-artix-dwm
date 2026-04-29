39d8b35b0033a42d34e768235a1f1943390454e2
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2026-04-29 13:41:45 +1000

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -4,24 +4,24 @@ cd /tmp
 git clone https://github.com/Connor-McCartney/deploy-artix-dwm
 
 
-mv /tmp/deploy-arch-dwm/dotfiles/.config/gtk-3.0 /home/connor/.config
-mv /tmp/deploy-arch-dwm/dotfiles/.config/kitty /home/connor/.config
-mv /tmp/deploy-arch-dwm/dotfiles/.config/konsolerc /home/connor/.config
-mv /tmp/deploy-arch-dwm/dotfiles/.local/share/konsole /home/connor/.local/share
-mv /tmp/deploy-arch-dwm/dotfiles/.config/nvim /home/connor/.config
-mv /tmp/deploy-arch-dwm/dotfiles/.config/picom /home/connor/.config
-mv /tmp/deploy-arch-dwm/dotfiles/.bashrc /home/connor
-mv /tmp/deploy-arch-dwm/dotfiles/.fdignore /home/connor
-mv /tmp/deploy-arch-dwm/dotfiles/pyproject.toml /home/connor
-mv /tmp/deploy-arch-dwm/dotfiles/.wallpapers /home/connor
+mv /tmp/deploy-artix-dwm/dotfiles/.config/gtk-3.0 /home/connor/.config
+mv /tmp/deploy-artix-dwm/dotfiles/.config/kitty /home/connor/.config
+mv /tmp/deploy-artix-dwm/dotfiles/.config/konsolerc /home/connor/.config
+mv /tmp/deploy-artix-dwm/dotfiles/.local/share/konsole /home/connor/.local/share
+mv /tmp/deploy-artix-dwm/dotfiles/.config/nvim /home/connor/.config
+mv /tmp/deploy-artix-dwm/dotfiles/.config/picom /home/connor/.config
+mv /tmp/deploy-artix-dwm/dotfiles/.bashrc /home/connor
+mv /tmp/deploy-artix-dwm/dotfiles/.fdignore /home/connor
+mv /tmp/deploy-artix-dwm/dotfiles/pyproject.toml /home/connor
+mv /tmp/deploy-artix-dwm/dotfiles/.wallpapers /home/connor
 
-mv /tmp/deploy-arch-dwm/dotfiles/.switcher.sh /home/connor
-mv /tmp/deploy-arch-dwm/dotfiles/.xsage.sh /home/connor
-mv /tmp/deploy-arch-dwm/dotfiles/.upvol.sh /home/connor
-mv /tmp/deploy-arch-dwm/dotfiles/.downvol.sh /home/connor
+mv /tmp/deploy-artix-dwm/dotfiles/.switcher.sh /home/connor
+mv /tmp/deploy-artix-dwm/dotfiles/.xsage.sh /home/connor
+mv /tmp/deploy-artix-dwm/dotfiles/.upvol.sh /home/connor
+mv /tmp/deploy-artix-dwm/dotfiles/.downvol.sh /home/connor
 
-mv /tmp/deploy-arch-dwm/dotfiles/.volume_change.mp3 /home/connor
+mv /tmp/deploy-artix-dwm/dotfiles/.volume_change.mp3 /home/connor
 
 chmod +x /home/connor/.switcher.sh /home/connor/.xsage.sh /home/connor/.upvol.sh /home/connor/.downvol.sh
 
-rm -rf /tmp/deploy-arch-dwm
+rm -rf /tmp/deploy-artix-dwm
