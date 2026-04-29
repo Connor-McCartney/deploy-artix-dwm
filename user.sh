c1fefd18d12c224ac834bed2c619ccfa91867cc7
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2026-04-29 10:49:34 +1000

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,5 +1,5 @@
 set -e
 
-sudo pacman -S --noconfirm base-devel os-prober grub vlc htop git wget ripgrep fzf bat fd vim less
+sudo pacman -S --noconfirm base-devel os-prober grub vlc htop git wget ripgrep fzf bat fd vim less pavucontrol
 
 sudo pacman -S --noconfirm xlibre-xserver
