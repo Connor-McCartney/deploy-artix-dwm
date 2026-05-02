a343c778703ac003d4975d75ae1da1065a8c0dbe
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2026-05-03 07:47:14 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-target="uefi-luks-hyperv"
+#target="uefi-luks-hyperv"
 #target="bios-thinkpad"
+target="uefi-luks-HP"
 
 
 ################
@@ -25,6 +26,25 @@ if [[ $target = "uefi-luks-hyperv" ]]; then
     mount /dev/sda1 /mnt/boot
     swapon /dev/vg1/swap
 fi
+
+
+if [[ $target = "uefi-luks-HP" ]]; then
+    printf "g\nn\n1\n\n+256M\nt\n1\nn\n2\n\n\nw\n" | fdisk nvme0n1
+    cryptsetup luksFormat /dev/nvme0n1p2
+    cryptsetup open /dev/nvme0n1p2 cryptlvm
+    pvcreate /dev/mapper/cryptlvm
+    vgcreate vg1 /dev/mapper/cryptlvm
+    lvcreate -L 8G vg1 -n swap
+    lvcreate -l 100%FREE vg1 -n root
+    mkfs.vfat -F 32 /dev/nvme0n1p1
+    mkfs.ext4 /dev/vg1/root
+    mkswap /dev/vg1/swap
+    mount /dev/vg1/root /mnt
+    mkdir /mnt/boot
+    mount /dev/nvme0n1p1 /mnt/boot
+    swapon /dev/vg1/swap
+fi
+
 
 
 if [[ $target = "bios-thinkpad" ]]; then
